015f435e6f465e26469be6485cb201cc7b424c0e
Semol <semolmahamud2233@gmail.com>
2021-05-26 15:37:16 +0600

    jdfh

--- backend_project/urls.py
+++ backend_project/urls.py
@@ -8,6 +8,6 @@ from django.conf.urls.static import static
 urlpatterns = [
   
     path('',include('Data_app.urls')),
-    path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls)
 
 ]
